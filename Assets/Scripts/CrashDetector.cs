943230f975e64a8ad76016f4618828a8101873f0
Dan Timmins <dantimmins97@gmail.com>
2024-03-28 19:48:21 -0400

    Prevent crash effects/sounds from playing more than once.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,10 +6,12 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float invokeDelay = 1f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashAudio;
+    bool hasCrashed = false;
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Ground")
+        if(other.tag == "Ground" && hasCrashed == false)
         {
+            hasCrashed = true;
             FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashAudio);

1c06c827b939b47ce4a1c15b81f04844213db8ba
Dan Timmins <dantimmins97@gmail.com>
2024-03-28 17:16:45 -0400

    Add sound effects for crash and finish.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,13 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float invokeDelay = 1f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashAudio;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashAudio);
             Invoke("ReloadScene", invokeDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,11 +5,13 @@ public class FinishLine : MonoBehaviour
 {
     [SerializeField] float invokeDelay = 2f;
     [SerializeField] ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
             finishEffect.Play();
+            GetComponent<AudioSource>().Play();
             Invoke("ReloadScene", invokeDelay);
         }      
     }
